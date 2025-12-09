2424e89e0ae6f84863bfc3576c18f5f1ffa59d47
aantle11 <alicia.antle@keyin.com>
2025-12-08 22:02:03 -0330

    bstapplication and pom.xml updated

--- src/main/java/com/alicia/bstapp/bstapp.java
+++ src/main/java/com/alicia/bstapp/bstapp.java
@@ -1,4 +1,11 @@
 package com.alicia.bstapp;
 
-public class bstapp {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bstappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(bstappApplication.class, args);
+    }
 }
